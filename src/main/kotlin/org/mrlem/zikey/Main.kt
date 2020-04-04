45d5d46a3275ea05d13bb8901b51fbf148757eb6
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2020-04-04 10:47:05 +0200

    sound: add basic javax sound keyboard to synth

--- src/main/kotlin/org/mrlem/zikey/Main.kt
+++ src/main/kotlin/org/mrlem/zikey/Main.kt
@@ -1,5 +1,22 @@
 package org.mrlem.zikey
 
+import javax.sound.midi.MidiSystem
+
 fun main() {
-    println("started")
+    println("creating synth")
+    val synthesizer = MidiSystem.getSynthesizer()
+    synthesizer.open()
+
+    println("loading default sound bank")
+    val soundBank = synthesizer.defaultSoundbank
+    synthesizer.loadAllInstruments(soundBank)
+
+    println("setting instrument")
+    val channels = synthesizer.channels
+    channels[0].programChange(12)
+
+    println("connecting keyboard to synth")
+    MidiSystem.getTransmitter().receiver = synthesizer.receiver
+
+    println("ready")
 }
